1edea47a397448bd0e47bd67483948c8b8e2a678
jhkim593 <fpdlwjzlr@naver.com>
2024-10-11 20:10:42 +0900

    상속과 조합

--- src/main/java/cleancode/minesweeper/tobe/cell/Cell.java
+++ src/main/java/cleancode/minesweeper/tobe/cell/Cell.java
@@ -1,30 +1,13 @@
 package cleancode.minesweeper.tobe.cell;
 
-public abstract class Cell {
-    protected static final String FLAG_SIGN = "⚑";
-    protected static final String UNCHECKED_SIGN = "□";
-    protected boolean isFlagged;
-    protected boolean isOpened;
-
-    public abstract String getSign();
-
-    public void flag() {
-        this.isFlagged = true;
-    }
-
-    public boolean isChecked() {
-        return isFlagged || isOpened;
-    }
-
-    public abstract boolean isLandMine();
-
-    public void open() {
-        this.isOpened = true;
-    }
-
-    public boolean isOpened() {
-        return this.isOpened;
-    }
-
-    public abstract boolean hasLandMineCount();
+public interface Cell {
+    static final String FLAG_SIGN = "⚑";
+    static final String UNCHECKED_SIGN = "□";
+    String getSign();
+    void flag();
+    boolean isChecked();
+    boolean isLandMine();
+    void open();
+    boolean isOpened();
+    boolean hasLandMineCount();
 }
--- src/main/java/cleancode/minesweeper/tobe/cell/EmptyCell.java
+++ src/main/java/cleancode/minesweeper/tobe/cell/EmptyCell.java
@@ -1,26 +1,46 @@
 package cleancode.minesweeper.tobe.cell;
 
-public class EmptyCell extends Cell {
+public class EmptyCell implements Cell {
 
     private static final String EMPTY_SIGN = "■";
+    private final CellState cellState = CellState.initialize();
     @Override
     public String getSign() {
-        if(isOpened){
+        if(cellState.isOpened()){
             return EMPTY_SIGN;
         }
-        if(isFlagged){
+        if(cellState.isFlagged()){
             return FLAG_SIGN;
         }
         return UNCHECKED_SIGN;
     }
 
     @Override
-    public boolean isLandMine() {
-        return false;
+    public void flag() {
+        cellState.flag();
     }
 
     @Override
+    public boolean isChecked() {
+        return cellState.isChecked();
+    }
+
+    @Override
+    public void open() {
+        cellState.open();
+    }
+
+    @Override
+    public boolean isOpened() {
+        return cellState.isOpened();
+    }
+    @Override
     public boolean hasLandMineCount() {
         return true;
+    }
+
+    @Override
+    public boolean isLandMine() {
+        return false;
     }
 }
